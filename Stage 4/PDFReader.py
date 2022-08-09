5e2e8f5659d8f9754794716e2da31cfd046414de
terrenceshang <90186971+terrenceshang@users.noreply.github.com>
2022-08-09 09:40:30 +0200

    program

--- Stage 4/PDFReader.py
+++ Stage 4/PDFReader.py
@@ -1,1 +1,8 @@
-print("Hello World")
+import PyPDF2
+
+pdfFileObj = open("C:\\Users\\Terrence Shang\\OneDrive - University of Cape Town\\Online Lecture\\CSC3003S\\Capstone Project\\CS-Capestone-Project\\Stage 4\\Area Central.pdf", "rb")
+pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
+print(pdfReader.numPages)
+pageObj = pdfReader.getPage(0)
+print(pageObj.extract_text())
+pdfFileObj.close()
