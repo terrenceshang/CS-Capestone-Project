655d3bbd420a4304c710b863c18793f36bed44e2
terrenceshang <90186971+terrenceshang@users.noreply.github.com>
2022-08-14 10:18:21 +0200

    PDF reader

--- Stage 4/PDFReader.py
+++ Stage 4/PDFReader.py
@@ -1,29 +1,43 @@
 import PyPDF2
 import io
 
+file = open('C:\\Users\\Terrence Shang\\OneDrive - University of Cape Town\\Online Lecture\\CSC3003S\\Capstone Project\\CS-Capestone-Project\\Stage 4\\trains.txt', 'w')
 pdfFileObj = open("C:\\Users\\Terrence Shang\\OneDrive - University of Cape Town\\Online Lecture\\CSC3003S\\Capstone Project\\CS-Capestone-Project\\PDFDoc\\Area Central.pdf", "rb")
 pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
 
-#print(pdfReader.numPages) reading number of pages
-pageObj = pdfReader.getPage(0)
-pageText = pageObj.extract_text()
+#Getting needed information from Area Central PDF to notepad
+for i in range (11):
 
-count = 0
-list = []
+    #Reading the PDF at page i
+    pageObj = pdfReader.getPage(i)
+    pageText = pageObj.extract_text()
 
-print (pageText)
-#Remove the last lines if it is "FROM MUTUAL"
-"""
-for line in io.StringIO(pageText):
-    if (line[0] == "R"):
-        list[count-1] = list[count-1][0:len(list[count-1])-2]
-        break
-    list.append(line)
-    count = count + 1
+    count = 0
+    days, departure = "", ""
+    platformNumber = trainNumber = departureTime = list = []
 
-count = 0
-for line in list:
-    print(str(count) + line)
-    count += 1
+    #Getting the important information needed for Notepad
+    for line in io.StringIO(pageText):
+        if (line.find("MONDAY TO FRIDAY")>=0):
+            days = "MF"
+        if (line[0:12] == "PLATFORM NO."):
+            platformNumber = line[13:].split(" ")
+        if (line[0:9] == "TRAIN NO."):
+            trainNumber = line[10:].split(" ")
+        
+        if (2<count<10) and (line[1:10]=="CAPE TOWN"):
+            departure = "Cape_Town"
+            departureTime = line[11:].split(" ")       
+        list.append(line)
+        count = count + 1
+
+    #Write the important information to the Notepad
+    file.write((days+" ")*len(trainNumber)+ "\n")
+    file.write((departure+" ")*len(trainNumber) + "\n")
+    file.write(" ".join(str(x) for x in departureTime))
+    file.write(" ".join(str(x) for x in platformNumber))
+    file.write(" ".join(str(x) for x in trainNumber))
+    
+
 pdfFileObj.close()
-"""
+file.close
