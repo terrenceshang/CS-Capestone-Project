e089286eac33e2be88a0605fbd9bc3eb0a6ccadd
Erin-Heath <100764816+Erin-Heath@users.noreply.github.com>
2022-08-18 11:05:39 +0200

    Update Search.py

--- Stage 4/Search.py
+++ Stage 4/Search.py
@@ -5,7 +5,7 @@ class Search:
         self.startTime = startTime
         self.endTime = endTime
 
-    def findRoutes():
+    def findRoutes(self):
         station = Stations()
         if station.findStation(self.destStation) = False:
             return "Could not find Station"
