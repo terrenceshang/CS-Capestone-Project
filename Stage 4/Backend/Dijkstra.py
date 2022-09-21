72ed322dc31b5c7cb594b27a361b51d71214444c
Erin-Heath <100764816+Erin-Heath@users.noreply.github.com>
2022-09-21 16:29:55 +0200

    Update Dijkstra.py

--- Stage 4/Backend/Dijkstra.py
+++ Stage 4/Backend/Dijkstra.py
@@ -41,72 +41,9 @@ class Graph(object):
     def value(self, node1, node2):
         "Returns the value of an edge between two nodes."
         return self.graph[node1][node2]
-
-def dijkstra_algorithm(graph, start_node):
-    unvisited_nodes = list(graph.get_nodes())
-    #print(unvisited_nodes)
- 
-    # We'll use this dict to save the cost of visiting each node and update it as we move along the graph   
-    shortest_path = {}
- 
-    # We'll use this dict to save the shortest known path to a node found so far
-    previous_nodes = {}
- 
-    # We'll use max_value to initialize the "infinity" value of the unvisited nodes   
-    max_value = sys.maxsize
-    for node in unvisited_nodes:
-        shortest_path[node] = max_value
-    # However, we initialize the starting node's value with 0   
-    shortest_path[start_node] = 0
     
-    # The algorithm executes until we visit all nodes
-    while unvisited_nodes:
-        # The code block below finds the node with the lowest score
-        current_min_node = None
-        for node in unvisited_nodes: # Iterate over the nodes
-            if current_min_node == None:
-                current_min_node = node
-            elif shortest_path[node] < shortest_path[current_min_node]:
-                current_min_node = node
-                
-        # The code block below retrieves the current node's neighbors and updates their distances
-        neighbors = graph.get_outgoing_edges(current_min_node)
-        #print(neighbors)
-        for neighbor in neighbors:
-            tentative_value = shortest_path[current_min_node] + graph.value(current_min_node, neighbor)
-            if tentative_value < shortest_path[neighbor]:
-                shortest_path[neighbor] = tentative_value
-                # We also update the best path to the current node
-                previous_nodes[neighbor] = current_min_node
- 
-        # After visiting its neighbors, we mark the node as "visited"
-        unvisited_nodes.remove(current_min_node)
-    
-    return previous_nodes, shortest_path
-
-def print_result(previous_nodes, shortest_path, start_node, target_node):
-    path = []
-    node = target_node
-    
-    while node != start_node:
-        path.append(node)
-        node = previous_nodes[node]
- 
-    # Add the start node manually
-    path.append(start_node)
-    
-    print("We found the following best path with a value of {}.".format(shortest_path[target_node]))
-    print(" -> ".join(reversed(path)))
-
-    strPath = ",".join(reversed(path))
-    arrPath = strPath.split(",")
-
-    #print ("Hello " + arrPath) 
-    return arrPath
-
-def main():
-
-    file = open(os.path.dirname(os.path.realpath(__file__)) + "\\Duration\\Area North Duration.txt", "r")
+def createGraph():
+    file = open("C:\\Users\\Erin\\Documents\\2. CSC3003S\\Project\\Backend\\Area North Duration.txt", "r")
     lstANDuration = []    
     for line in file:        
         lstANDuration.append ((line[:-1].split(",")))        
@@ -178,21 +115,93 @@ def main():
         if len(i) > 1:
             #print(i)
             init_graph[i[0]][i[1]] = int(i[2])
-
+            
+    graph = Graph(nodes, init_graph)
     
+    return graph, lstACDuration, lstACRoute, lstANDuration, lstANRoute, lstASDuration, lstASRoute, lstStation, StationsArea
+
+def dijkstra_algorithm(graph, start_node):
+    unvisited_nodes = list(graph.get_nodes())
+    #print(unvisited_nodes)
+ 
+    # We'll use this dict to save the cost of visiting each node and update it as we move along the graph   
+    shortest_path = {}
+ 
+    # We'll use this dict to save the shortest known path to a node found so far
+    previous_nodes = {}
+ 
+    # We'll use max_value to initialize the "infinity" value of the unvisited nodes   
+    max_value = sys.maxsize
+    for node in unvisited_nodes:
+        shortest_path[node] = max_value
+    # However, we initialize the starting node's value with 0   
+    shortest_path[start_node] = 0
+    
+    # The algorithm executes until we visit all nodes
+    while unvisited_nodes:
+        # The code block below finds the node with the lowest score
+        current_min_node = None
+        for node in unvisited_nodes: # Iterate over the nodes
+            if current_min_node == None:
+                current_min_node = node
+            elif shortest_path[node] < shortest_path[current_min_node]:
+                current_min_node = node
+                
+        # The code block below retrieves the current node's neighbors and updates their distances
+        neighbors = graph.get_outgoing_edges(current_min_node)
+        #print(neighbors)
+        for neighbor in neighbors:
+            tentative_value = shortest_path[current_min_node] + graph.value(current_min_node, neighbor)
+            if tentative_value < shortest_path[neighbor]:
+                shortest_path[neighbor] = tentative_value
+                # We also update the best path to the current node
+                previous_nodes[neighbor] = current_min_node
+ 
+        # After visiting its neighbors, we mark the node as "visited"
+        unvisited_nodes.remove(current_min_node)
+    
+    return previous_nodes, shortest_path
+
+def print_result(previous_nodes, shortest_path, start_node, target_node):
+    path = []
+    node = target_node
+    
+    while node != start_node:
+        path.append(node)
+        node = previous_nodes[node]
+ 
+    # Add the start node manually
+    path.append(start_node)
+    
+    print("We found the following best path with a value of {}.".format(shortest_path[target_node]))
+    print(" -> ".join(reversed(path)))
+
+    strPath = ",".join(reversed(path))
+    arrPath = strPath.split(",")
+
+    return arrPath
+
+def findRoute(start_node, end_node):
+    graph, lstACDuration, lstACRoute, lstANDuration, lstANRoute, lstASDuration, lstASRoute, lstStation, StationsArea = createGraph()
+    previous_nodes, shortest_path = dijkstra_algorithm(graph=graph, start_node = start_node)
+    arrPath = print_result(previous_nodes, shortest_path, start_node=start_node, target_node= end_node)
+    
+    return arrPath
+
+def main():       
     found = False
     while found == False:
-        graph = Graph(nodes, init_graph)
+        graph, lstACDuration, lstACRoute, lstANDuration, lstANRoute, lstASDuration, lstASRoute, lstStation, StationsArea = createGraph()
         previous_nodes, shortest_path = dijkstra_algorithm(graph=graph, start_node="Chris Hani")
-        arrPath = print_result(previous_nodes, shortest_path, start_node="Chris Hani", target_node="Langa")
+        arrPath = print_result(previous_nodes, shortest_path, start_node="Chris Hani", target_node="Du Toit")
         
         bArea = False
         for i in arrPath:        
             if bArea == False:
                 if len(StationsArea[i]) == 1:
                     area = StationsArea[i]
-                    bArea = True
-                
+                    bArea = True                
+        
         route = []
         if area == "C":
             route = lstACRoute
@@ -201,24 +210,34 @@ def main():
         else:
             route = lstASRoute
         
+        matches = []
+        routeName = ""
         for lineRoute in route:
+            routeName = lineRoute[0:1]
             for linePath in arrPath:
                 if StationsArea[linePath] == area:
                     bPathFound = False
                     count = 0
+                    
                     for i in lineRoute:
                         count += 1
                         if i == linePath:
                             bPathFound = True                             
                             lineRoute = lineRoute[count:len(lineRoute)]
-                            count = 0
+                            count = 0                            
+                            #print(lineRoute)
+                            if linePath == arrPath[arrPath.__len__()-1]:
+                                matches.append(routeName)
                             break
                 else:
                     print("path found")
+                    matches.append(routeName)
                     break
                 if bPathFound == False:
                     break
         found = True
+        
+        print(matches)
 
 if __name__ == "__main__":
     main()
